17122764d6e766c2db944a6cded527e9bf59b06a
pedrobarros <wipuliwo@gmail.com>
2018-07-26 19:14:44 +0100

    ..

--- core/src/org/academiadecodigo/bootcamp/simulation/maps/Map.java
+++ core/src/org/academiadecodigo/bootcamp/simulation/maps/Map.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 public interface Map {
 
     TiledMap getMap (int index);
-    void add();
-    void remove();
+    void add(TiledMap map);
+    void remove(int index);
 
 }

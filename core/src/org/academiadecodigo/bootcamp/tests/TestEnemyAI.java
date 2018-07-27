a67bbe6613810821f2e0ad7fd102093898477f99
pedrobarros <wipuliwo@gmail.com>
2018-07-27 11:16:09 +0100

    doneherebitch

--- core/src/org/academiadecodigo/bootcamp/tests/TestEnemyAI.java
+++ core/src/org/academiadecodigo/bootcamp/tests/TestEnemyAI.java
@@ -24,7 +24,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TestEnemyAI extends ApplicationAdapter {
-
+git a
     private Map maps;
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
